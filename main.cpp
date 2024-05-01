2679157ae95f19fbcf3b7ff01d4cd108453da112
Petrenko Valeriya <mroom9195@gmail.com>
2024-05-01 19:27:26 +0300

    added ex1

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main() {
 	srand(time(0));
 	const int MIN_NUMBER = 1;
 	const int MAX_NUMBER = 500;
-	int secretNumber = rand() % 1 + 499;
+	int secretNumber = rand() % 499 + 1;
 	int guess;
+	time_t startTime, endTime;
 	cout << "Guess the number from " << MIN_NUMBER << " to " << MAX_NUMBER << endl;
 	cout << "Enter 0 to exit the game." << endl;
+	startTime = time(0);
 	for (size_t i = 1; ; i++)
 	{
 		cout << "Attempt " << i << ": ";
@@ -27,6 +29,8 @@ int main() {
 			break;
 		}
 	}
+	endTime = time(0);
+	cout << "Time spent on the game " << (endTime - startTime) << " seconds" << endl;
 	cout << "You guessed the number " << secretNumber << "!" << endl;
 	return 0;
 }
