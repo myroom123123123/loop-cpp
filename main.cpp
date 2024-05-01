1fbeb522da3ff517db6b10dcf11e2f8aee01c568
Petrenko Valeriya <mroom9195@gmail.com>
2024-05-01 20:36:21 +0300

    added ex5 hm

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     int a;
-    cout << "Enter an integer A: ";
+    cout << "Enter an integer a: ";
     cin >> a;
-    cout << "Integers B, for which " << a << " is divisible by B*B but not divisible by B*B*B:" << endl;
-    for (int b = 1; b <= a; ++b) {
-        if (a % (b * b) == 0 && a % (b * b * b) != 0) {
-            cout << b << endl;
-        }
+    int sum = 0;
+    for (int i = a; i != 0; i /= 10) {
+        sum += i % 10;
+    }
+    int cube = sum * sum * sum;
+    if (cube == a * a) {
+        cout << "The cube of the sum of the digits of " << a << " is equal to " << a * a << endl;
+    }
+    else {
+        cout << "The cube of the sum of the digits of " << a << " is not equal to " << a * a << endl;
     }
     return 0;
 }
