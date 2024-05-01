1db31784bb7f1529bb1053c58fe655c9e5af71e3
Petrenko Valeriya <mroom9195@gmail.com>
2024-05-01 20:47:55 +0300

    added ex7 hm

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main() {
-    int number;
-    cout << "Enter an integer: ";
-    cin >> number;
-    cout << "The number " << number << " divides evenly into: ";
-    for (int i = 1; i <= number; ++i) {
-        if (number % i == 0) {
+    int number1, number2;
+    cout << "Enter the first integer: ";
+    cin >> number1;
+    cout << "Enter the second integer: ";
+    cin >> number2;
+    cout << "Common divisors of " << number1 << " and " << number2 << ": ";
+    for (int i = 1; i <= min(number1, number2); ++i) {
+        if (number1 % i == 0 && number2 % i == 0) {
             cout << i << " ";
         }
     }
